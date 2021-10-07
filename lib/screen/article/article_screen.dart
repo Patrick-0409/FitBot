48883b866185e8c06610a44facb23c61732d2fab
Patrick-0409 <patrick.pasaribu0904@gmail.com>
2021-10-07 15:15:58 +0700

    Fix Container and Add Favorite Article

--- lib/screen/article/article_screen.dart
+++ lib/screen/article/article_screen.dart
@@ -1,72 +1,75 @@
 import 'package:fiton/constant.dart';
 import 'package:fiton/screen/authentication/login/login_screen.dart';
+import 'package:fiton/screen/homepage/home_screen.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-import 'components/body.dart';
+import 'components/favorite_body.dart';
+import 'components/trending_body.dart';
 
 class ArticleScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Scaffold(
-      backgroundColor: kBackgroundColor,
-      appBar: PreferredSize(
-        preferredSize: Size.fromHeight(85.0),
-        child: Padding(
-          padding: EdgeInsets.only(top: 11),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              ListTile(
-                title: Text(
-                  "FitOn",
-                  style: Theme.of(context).textTheme.headline5,
-                ),
-                trailing: IconButton(
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        backgroundColor: kBackgroundColor,
+        appBar: PreferredSize(
+          preferredSize: Size.fromHeight(100.0),
+          child: Padding(
+            padding: EdgeInsets.only(top: 11),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                ListTile(
+                  title: Text(
+                    "FitOn",
+                    textAlign: TextAlign.start,
+                    style: Theme.of(context).textTheme.headline5,
+                  ),
+                  trailing: IconButton(
                     icon: Icon(
                       Icons.home,
                       color: Colors.black,
-                      size: 35,
                     ),
                     onPressed: () {
                       Navigator.push(
                         context,
                         MaterialPageRoute(
                           builder: (context) {
-                            return LoginScreen();
+                            return HomeScreen();
                           },
                         ),
                       );
-                    }),
-              ),
-              Padding(
-                padding: EdgeInsets.only(left: 17),
-                child: Text(
-                  "Trending",
-                  style: Theme.of(context).textTheme.bodyText2,
+                    },
+                  ),
                 ),
-              ),
-              // Align(
-              //   alignment: Alignment.topLeft,
-              //   child: TabBar(
-              //     labelColor: Colors.white,
-              //     // unselectedLabelColor: Colors.black54,
-              //     // indicatorSize: TabBarIndicatorSize.label,
-              //     indicatorColor: Colors.,
-              //     isScrollable: true,
-              //     labelStyle: TextStyle(fontSize: 20),
-              //     tabs: [
-              //       Tab(text: "Trending"),
-              //     ],
-              //   ),
-              // )
-            ],
+                TabBar(
+                  labelColor: Colors.white,
+                  unselectedLabelColor: kGrey1,
+                  unselectedLabelStyle: kNonActiveTabStyle,
+                  // indicatorSize: TabBarIndicatorSize.label,
+                  // isScrollable: true,
+                  indicatorColor: Colors.white,
+                  labelStyle: kActiveTabStyle.copyWith(fontSize: 20.0),
+                  tabs: [
+                    Tab(text: "Trending"),
+                    Tab(text: "Favorite"),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
+        body: TabBarView(
+          children: [
+            Container(child: TrendingBody()),
+            Container(child: FavoriteBody()),
+          ],
+        ),
       ),
-      body: Body(),
     );
   }
 }
