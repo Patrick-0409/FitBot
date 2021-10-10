6af79d24274dc76d057ee684571f0ffbb1ff2022
Patrick-0409 <72242964+Patrick-0409@users.noreply.github.com>
2021-10-10 22:47:40 +0700

    Homepage Build

--- lib/screen/homepage/components/body.dart
+++ lib/screen/homepage/components/body.dart
@@ -1,9 +1,12 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:fiton/constant.dart';
 import 'package:fiton/screen/article/article_screen.dart';
 import 'package:fiton/screen/provider/google_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+
+import '../home_screen.dart';
 
 class Body extends StatelessWidget {
   final FirebaseAuth firebaseAuth = FirebaseAuth.instance;
@@ -12,64 +15,48 @@ class Body extends StatelessWidget {
   Widget build(BuildContext context) {
     final user = FirebaseAuth.instance.currentUser!;
 
-    return Scaffold(
-      appBar: AppBar(
-        automaticallyImplyLeading: false,
-        title: Text('Logged In'),
-        centerTitle: true,
-        actions: [
-          TextButton(
-            child: Text(
-              'Logout',
-              style: TextStyle(color: Colors.red),
-            ),
-            onPressed: () {
-              final provider =
-                  Provider.of<GoogleSignInProvider>(context, listen: false);
-              provider.logout();
-            },
-          ),
-        ],
-      ),
-      body: Container(
-        alignment: Alignment.center,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            if (user.photoURL != null)
-              CircleAvatar(
-                maxRadius: 40,
-                backgroundImage: NetworkImage(user.photoURL!),
+    return SafeArea(
+      child: SingleChildScrollView(
+        child: Container(
+          alignment: Alignment.center,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              if (user.photoURL != null)
+                CircleAvatar(
+                  maxRadius: 40,
+                  backgroundImage: NetworkImage(user.photoURL!),
+                ),
+              SizedBox(height: 20),
+              if (user.displayName != null)
+                Text(
+                  'Hello, ' + user.displayName!,
+                  style: TextStyle(fontSize: 36, color: Colors.black),
+                ),
+              SizedBox(
+                height: 20,
               ),
-            SizedBox(height: 20),
-            if (user.displayName != null)
               Text(
-                'Hello, ' + user.displayName!,
-                style: TextStyle(fontSize: 36, color: Colors.black),
+                user.email!,
+                style: TextStyle(fontSize: 20, color: Colors.black),
               ),
-            SizedBox(
-              height: 20,
-            ),
-            Text(
-              user.email!,
-              style: TextStyle(fontSize: 20, color: Colors.black),
-            ),
-            IconButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) {
-                      return ArticleScreen();
-                    },
-                  ),
-                );
-              },
-              icon: Icon(
-                Icons.article,
+              IconButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) {
+                        return ArticleScreen();
+                      },
+                    ),
+                  );
+                },
+                icon: Icon(
+                  Icons.article,
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
--- lib/screen/homepage/home_screen.dart
+++ lib/screen/homepage/home_screen.dart
@@ -1,12 +1,64 @@
+import 'package:fiton/screen/article/article_screen.dart';
 import 'package:flutter/material.dart';
 
+import '../../constant.dart';
 import 'components/body.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return Scaffold(
+      backgroundColor: kBackgroundColor,
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(45.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            ListTile(
+              leading: IconButton(
+                icon: Icon(
+                  Icons.home,
+                  color: Colors.black,
+                  size: 35,
+                ),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) {
+                        return ArticleScreen();
+                      },
+                    ),
+                  );
+                },
+              ),
+              title: Text(
+                "FitOn",
+                textAlign: TextAlign.center,
+                style: Theme.of(context).textTheme.headline5,
+              ),
+              trailing: IconButton(
+                icon: Icon(
+                  Icons.home,
+                  color: Colors.black,
+                  size: 35,
+                ),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) {
+                        return ArticleScreen();
+                      },
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
       body: Body(),
     );
   }
